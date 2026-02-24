a32c5be19b70e1228ec514e6297f64fe4d9321c6
Chrystian Dybas <dybas.chrys@gmail.com>
2026-02-24 12:51:29 +0000

    chat model and controller

---
+++ src/main/java/com/example/hub/model/ChatMessage.java
@@ -0,0 +1,18 @@
+package com.example.hub.model;
+
+public class ChatMessage {
+    private String content;
+    private String username;
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
